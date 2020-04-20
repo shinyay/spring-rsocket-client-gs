edb5489b41cc29511d4369a181551d724e113a5c
shinyay <shinya.com@gmail.com>
2020-04-20 22:51:32 +0900

    Add RSocketRequester

--- spring/src/main/kotlin/com/google/shinyay/client/RSocketShellClient.kt
+++ spring/src/main/kotlin/com/google/shinyay/client/RSocketShellClient.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.client
 
+import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.shell.standard.ShellComponent
 
 @ShellComponent
-class RSocketShellClient {
+class RSocketShellClient(val rsocketRequestBuilder: RSocketRequester.Builder) {
+
+    val rSocketRequester: RSocketRequester? = rsocketRequestBuilder.connectTcp("localhost", 7000).block()
 }
