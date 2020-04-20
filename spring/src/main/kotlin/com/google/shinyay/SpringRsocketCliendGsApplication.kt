8cfe0a09d1844d9564e21392acf9b90dc7560209
shinyay <shinya.com@gmail.com>
2020-04-21 06:14:47 +0900

    Add ShellMethod for request-and-response

--- spring/src/main/kotlin/com/google/shinyay/SpringRsocketCliendGsApplication.kt
+++ spring/src/main/kotlin/com/google/shinyay/SpringRsocketCliendGsApplication.kt
@@ -1,5 +1,7 @@
 package com.google.shinyay
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,3 +11,6 @@ class SpringRsocketCliendGsApplication
 fun main(args: Array<String>) {
 	runApplication<SpringRsocketCliendGsApplication>(*args)
 }
+
+val Any.logger: Logger
+	get() = LoggerFactory.getLogger(this.javaClass)
--- spring/src/main/kotlin/com/google/shinyay/client/RSocketShellClient.kt
+++ spring/src/main/kotlin/com/google/shinyay/client/RSocketShellClient.kt
@@ -1,10 +1,30 @@
 package com.google.shinyay.client
 
+import com.google.shinyay.logger
+import com.google.shinyay.model.Message
 import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
 
 @ShellComponent
 class RSocketShellClient(val rsocketRequestBuilder: RSocketRequester.Builder) {
 
-    val rSocketRequester: RSocketRequester? = rsocketRequestBuilder.connectTcp("localhost", 7000).block()
+    val client = "Client"
+    val request = "Request"
+    val fire_and_forget = "Fire-And-Forget"
+    val stream = "Stream"
+    val channel = "Channel"
+
+    val rsocketRequester: RSocketRequester? = rsocketRequestBuilder.connectTcp("localhost", 7000).block()
+
+    @ShellMethod
+    fun requestResponse(): Unit {
+        logger.info("Sending one request. Waiting for one response...")
+        val message = this.rsocketRequester
+                ?.route("request-response")
+                ?.data(Message(client, request))
+                ?.retrieveMono(Message::class.java)
+                ?.block()
+        logger.info("Response was: $message")
+    }
 }
